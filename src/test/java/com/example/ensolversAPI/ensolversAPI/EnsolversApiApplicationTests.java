21525fab8dcdbc925c68c3173e892699b1a41ab0
Juan Cruz Lafranconi <jlafranconi@uade.edu.ar>
2022-04-20 12:19:21 -0300

    first commit

---
+++ src/test/java/com/example/ensolversAPI/ensolversAPI/EnsolversApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ensolversAPI.ensolversAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsolversApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
